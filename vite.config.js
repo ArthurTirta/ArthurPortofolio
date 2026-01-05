db3c37527971cb39a9af0cc40944de62cb8350f9
ArthurTirta <arthur.tirtajaya@gmail.com>
2026-01-05 16:46:23 +0800

    backup dulu

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [  tailwindcss(),react()],
+<<<<<<< HEAD
   base: process.env.VITE_BASE_PATH ||"/ArthurPortofolio"||"/ArthurPorto2", 
+=======
+  base: "/ArthurPortofolio", 
+>>>>>>> f767a81ee927cb37089f8449ff0f28ce7569bc0f
 })
